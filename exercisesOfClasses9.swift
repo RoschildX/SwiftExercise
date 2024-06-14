14e879ae1df17fa606483c6ac38478c95183702d
farhan <mohamad.farhan@mcruncher.com>
2024-06-14 16:46:04 +0800

    Ball Class

--- exercisesOfClasses9.swift
+++ exercisesOfClasses9.swift
@@ -2,6 +2,8 @@
 The Ball class can be used in various scenarios, particularly in simulations, games, and animations where objects need to move, collide, and reflect off surfaces.
 
 
+// TODO: Ball Class
+
 class Ball {
     private var x: Float
     private var y: Float
@@ -75,11 +77,12 @@ class Ball {
     }
 }
 
+// Example usage
 var ball1 = Ball(x: 1.1, y: 2.2, radius: 5, xDelta: 3.3, yDelta: 4.4)
-print(ball1.toString())  // Should print: Ball[(1.1,2.2),speed=(3.3,4.4)]
+print(ball1.toString())  
 
 var ball2 = Ball(x: 80.0, y: 35.0, radius: 5, xDelta: 4.0, yDelta: 6.0)
-print(ball2.toString())  // Should print: Ball[(80.0,35.0),speed=(4.0,6.0)]
+print(ball2.toString())  
 
 print("x is: \(ball2.getX())")
 print("y is: \(ball2.getY())")
@@ -88,36 +91,33 @@ print("xDelta is: \(ball2.getXDelta())")
 print("yDelta is: \(ball2.getYDelta())")
 
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(84.0,41.0),speed=(4.0,6.0)]
+print(ball2.toString())  
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(88.0,47.0),speed=(4.0,6.0)]
+print(ball2.toString()) 
 ball2.reflectionVertical()
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(92.0,41.0),speed=(4.0,-6.0)]
+print(ball2.toString()) 
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(96.0,35.0),speed=(4.0,-6.0)]
+print(ball2.toString())  
 
 ball2.reflectionHorizontal()
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(92.0,29.0),speed=(-4.0,-6.0)]
+print(ball2.toString())  
+print(ball2.toString())  
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(88.0,23.0),speed=(-4.0,-6.0)]
+print(ball2.toString()) 
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(84.0,17.0),speed=(-4.0,-6.0)]
+print(ball2.toString())
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(80.0,11.0),speed=(-4.0,-6.0)]
-ball2.move()
-print(ball2.toString())  // Should print: Ball[(76.0,5.0),speed=(-4.0,-6.0)]
-ball2.move()
-print(ball2.toString())  // Should print: Ball[(72.0,-1.0),speed=(-4.0,-6.0)]
+print(ball2.toString()) 
+print(ball2.toString())  
 ball2.reflectionVertical()
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(68.0,5.0),speed=(-4.0,6.0)]
+print(ball2.toString())  
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(64.0,11.0),speed=(-4.0,6.0)]
+print(ball2.toString())  
+print(ball2.toString())  
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(60.0,17.0),speed=(-4.0,6.0)]
+print(ball2.toString())  
 ball2.move()
-print(ball2.toString())  // Should print: Ball[(56.0,23.0),speed=(-4.0,6.0)]
-ball2.move()
-print(ball2.toString())  // Should print: Ball[(52.0,29.0),speed=(-4.0,6.0)]
+print(ball2.toString())  
