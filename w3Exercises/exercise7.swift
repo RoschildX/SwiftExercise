ca553e775f33e6aa1ee346429882f7de221f9e30
farhan <mohamad.farhan@mcruncher.com>
2024-06-24 12:01:33 +0800

    Sortable Protocol

--- w3Exercises/exercise7.swift
+++ w3Exercises/exercise7.swift
@@ -1,21 +1,53 @@
-protocol Playable {
-    func play()
+import Foundation
+
+protocol Sortable {
+    func sort(_ array: inout [Int])
 }
 
-class Football {
-    func draw() {
-        print("Playing football")
+class BubbleSort: Sortable {
+    func sort(_ array: inout [Int]) {
+        let n = array.count
+        for i in 0..<n {
+            for j in 0..<(n - i - 1) {
+                if array[j] > array[j + 1] {
+                    let temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+                }
+            }
+        }
     }
 }
 
-class Volleyball {
-    func draw() {
-        print("Playing volleyball")
+class SelectionSort: Sortable {
+    func sort(_ array: inout [Int]) {
+        let n = array.count
+        for i in 0..<n {
+            var minIndex = i
+            for j in (i + 1)..<n {
+                if array[j] < array[minIndex] {
+                    minIndex = j
+                }
+            }
+            if i != minIndex {
+                let temp = array[i]
+                array[i] = array[minIndex]
+                array[minIndex] = temp
+            }
+        }
     }
 }
 
-class Basketaball {
-    func draw() {
-       print("Playing Basketball")
-    }
-}
+
+var bubbleSortArray = [64, 34, 25, 12, 22, 11, 90]
+var selectionSortArray = [64, 34, 25, 12, 22, 11, 90]
+
+let bubbleSorter: Sortable = BubbleSort()
+let selectionSorter: Sortable = SelectionSort()
+
+bubbleSorter.sort(&bubbleSortArray)
+selectionSorter.sort(&selectionSortArray)
+
+print("Bubble Sorted Array: \(bubbleSortArray)")
+print("Selection Sorted Array: \(selectionSortArray)")
+
