85b857c86c74a3b84dc681c1754ac92b1fbe24c6
farhan <mohamad.farhan@mcruncher.com>
2024-06-24 09:57:18 +0800

    Playable Protocol

---
+++ w3Exercises/exercise7.swift
@@ -0,0 +1,21 @@
+protocol Playable {
+    func play()
+}
+
+class Football {
+    func draw() {
+        print("Playing football")
+    }
+}
+
+class Volleyball {
+    func draw() {
+        print("Playing volleyball")
+    }
+}
+
+class Basketaball {
+    func draw() {
+       print("Playing Basketball")
+    }
+}
