c811f16cfa94aa1a66810a51a81d6b3a21e9ec6f
farhan <mohamad.farhan@mcruncher.com>
2024-06-24 09:45:19 +0800

    Drawable Protocol

---
+++ w3Exercises/exercise6.swift
@@ -0,0 +1,21 @@
+protocol Drawable {
+    func draw()
+}
+
+class Circle {
+    func draw() {
+        print("Circle")
+    }
+}
+
+class Rectangle {
+    func draw() {
+        print("Rectangle")
+    }
+}
+
+class Triangle {
+    func draw() {
+        print("Triangle")
+    }
+}
