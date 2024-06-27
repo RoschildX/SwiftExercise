ebc856d3adb7d7c97c684194c138eadbd7b57e9c
farhan <mohamad.farhan@mcruncher.com>
2024-06-27 15:39:30 +0800

    Optional force unwrapping

---
+++ optionalExercises/exercise2.swift
@@ -0,0 +1,6 @@
+//TODO: Optional force unwrapping
+
+var age: Int? = 25
+
+// Force unwrapping
+print("The age is \(age!)")
---
+++ optionalExercises/exercise3.swift
@@ -0,0 +1,15 @@
+// TODO: Optional chaining
+
+class Person {
+    var address: String?
+}
+
+let person = Person()
+person.address = "123 Main Street"
+
+// Optional chaining
+if let address = person.address {
+    print("The address is \(address)")
+} else {
+    print("No address provided")
+}
